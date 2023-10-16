4e54df8c1bddbec89ac6ae0158932b227fe28045
Wahyuy21 <wahyu.pratama@asyst.co.id>
2023-10-16 16:33:30 +0700

    TPMODUL0

--- TPMODUL0_WAHYU/main.java
+++ TPMODUL0_WAHYU/main.java
@@ -2,7 +2,7 @@
 public class main {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello World" );
+        System.out.println("Hello World!!!!" );
 
     }
 }
